bd781dc4d14f9f68a177e46b3a893b156b81cf46
Jayson Gazeley <jayson.gazeley@live.com>
2022-11-03 21:42:45 -0700

    First commit

---
+++ same.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[])
+{
+	if(argc != 3)								// validate user input
+	{
+		fprintf(stderr, "Usage: %s file1 file2", argv[0]);
+		exit(1);
+	}
+	
+	FILE* f1 = fopen(argv[1], "rb");			// open first file
+	if(!f1)
+	{
+		char errmsg[500];
+		sprintf(errmsg, "Cannot open %s for reading", argv[1]);
+		perror(errmsg);
+		exit(1);
+	}
+
+	FILE* f2 = fopen(argv[2], "rb");			// open second file
+	if(!f2)
+	{
+		char errmsg[500];
+		sprintf(errmsg, "Cannot open %s for reading", argv[2]);
+		perror(errmsg);
+		exit(1);
+	}
+
+	while(!feof(f1))
+	{
+		if(getc(f1) != getc(f2))
+		{
+			printf("The files are not identical.");
+			fclose(f1); fclose(f2);
+			exit(0);
+		}
+	}
+
+	printf("The files are identical.");
+	fclose(f1); fclose(f2);
+}
